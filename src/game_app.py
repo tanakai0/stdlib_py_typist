b3829d76694ff765620c54ec4c2f3f70a18b85a8
tanakai0 <tanakatanakakai@gmail.com>
2023-09-06 03:08:02 +0900

    add: selection of database

--- src/game_app.py
+++ src/game_app.py
@@ -19,21 +19,45 @@ class TypingGameApp:
         self.on_windows_os = platform.system() == "Windows"
         self.sound_available = self.on_windows_os
         self.widgets = []
-        self.quizzes = StaticQuizzes(database_path)
-
-        self.root = root
-        self.root.title("Typing Game")
-        # self.root.geometry('800x400')
-        self.mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+        self.quizzes = None
+        self.question = ""
 
         if self.sound_available:
             self.correct_sound = "./assets/sound/pinpon2.wav"
             self.incorrect_sound = "./assets/sound/bubbu1.wav"
             self.title_music = "./assets/sound/scene3.wav"
 
-        self.question = ""
+        self.root = root
+        self.root.title("Typing Game")
+        # self.root.geometry('800x400')
+        self.mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
 
         # widgets in the title screen
+        db_paths = [p for p in DATABASE_FOLDER.glob("*.db")]
+        self.quizzes_list = [StaticQuizzes(p) for p in db_paths]
+        self.overview2index = {(q.name, q.description): q for q in self.quizzes_list}
+
+        name = "名前"
+        description = "説明"
+        column = (name, description)
+        # style = ttk.Style()
+        # style.configure("Treeview", rowheight=40)
+        self.quizzes_table = ttk.Treeview(self.mainframe, columns=column, height=5, selectmode="browse")
+        self.quizzes_table.bind("<<TreeviewSelect>>", self.select_quizzes)
+        self.quizzes_table.column("#0", width=0, stretch="no")
+        self.quizzes_table.column(name, anchor="w", width=200)
+        self.quizzes_table.column(description, anchor="w", width=300)
+        self.quizzes_table.heading("#0", text="")
+        self.quizzes_table.heading(name, text=name, anchor="center")
+        self.quizzes_table.heading(description, text=description, anchor="center")
+        for i, (n, d) in enumerate(self.overview2index.keys()):
+            wrapped_n = "\n".join(textwrap.wrap(n, width=20))
+            wrapped_d = "\n".join(textwrap.wrap(d, width=20))
+            self.quizzes_table.insert(parent="", index="end", iid = i, values=(wrapped_n, wrapped_d))
+        if db_paths:
+            self.quizzes_table.selection_set(0)
+            self.quizzes_table.focus(0)
+            self.quizzes = self.quizzes_list[0]
         self.title_label = tk.Label(
             self.mainframe, text="Typing Game", font=("Helvetica", 36)
         )
@@ -101,7 +125,6 @@ class TypingGameApp:
             self.mainframe, textvariable=self.player_input, font=("Helvetica", 18)
         )
         self.player_input_entry.bind("<Return>", self.check_text)
-        self.new_quiz()
 
         self.set_title_screen()
 
@@ -127,6 +150,7 @@ class TypingGameApp:
             winsound.PlaySound(None, winsound.SND_PURGE)
 
     def push_quiz_start_button(self) -> None:
+        self.new_quiz()
         quiz_mode = self.quiz_mode.get()
         print(self.max_seconds.get())
         print(
@@ -152,29 +176,7 @@ class TypingGameApp:
 
     def set_mode_selection_screen(self) -> None:
         self.unset_screen()
-        db_paths = [p for p in DATABASE_FOLDER.glob("*.db")]
-        quizzes = [StaticQuizzes(p) for p in db_paths]
-        overview2index = {(q.name, q.description): q for q in quizzes}
-
-        # set table
-        name = "名前"
-        description = "説明"
-        column = (name, description)
-
-        # style = ttk.Style()
-        # style.configure("Treeview", rowheight=40)
-        quiz_table = ttk.Treeview(self.mainframe, columns=column, height=5)
-        quiz_table.column("#0", width=0, stretch="no")
-        quiz_table.column(name, anchor="w", width=200)
-        quiz_table.column(description, anchor="w", width=300)
-        quiz_table.heading("#0", text="")
-        quiz_table.heading(name, text=name, anchor="center")
-        quiz_table.heading(description, text=description, anchor="center")
-        for n, d in overview2index.keys():
-            wrapped_n = "\n".join(textwrap.wrap(n, width=20))
-            wrapped_d = "\n".join(textwrap.wrap(d, width=20))
-            quiz_table.insert(parent="", index="end", values=(wrapped_n, wrapped_d))
-        quiz_table.grid(column=0, row=0, columnspan=5, padx=10, pady=20)
+        self.quizzes_table.grid(column=0, row=0, columnspan=5)
         self.quiz_mode_label.grid(column=0, row=1, rowspan=3)
         self.endless_quiz_radio_button.grid(column=1, row=1, sticky=self.W)
         self.time_limit_radio_button.grid(column=1, row=2, sticky=self.W)
@@ -184,7 +186,7 @@ class TypingGameApp:
         self.quiz_start_button.grid(column=4, row=1, rowspan=3)
         self.title_back_button.grid(column=3, row=1, rowspan=3)
         self.widgets += [
-            quiz_table,
+            self.quizzes_table,
             self.quiz_mode_label,
             self.endless_quiz_radio_button,
             self.time_limit_radio_button,
@@ -194,6 +196,14 @@ class TypingGameApp:
             self.title_back_button,
             self.quiz_start_button,
         ]
+
+    def select_quizzes(self, event):
+        selected_id = self.quizzes_table.selection()
+        if not(selected_id):
+            return
+        _id = selected_id[0]
+        overview = self.quizzes_table.item(_id, "values")
+        self.quizzes = self.overview2index[overview]
 
     def set_endless_quiz_screen(self) -> None:
         self.unset_screen()
@@ -218,13 +228,13 @@ class TypingGameApp:
     def check_text(self, event) -> None:
         player_answer = self.player_input.get()
         if self.quiz.check_answer(player_answer):
-            self.text_label.config(text="Correct!", fg="green")
+            self.text_label.config(text="正解！", fg="green")
             if self.sound_available:
                 winsound.PlaySound(
                     self.correct_sound, winsound.SND_FILENAME | winsound.SND_ASYNC
                 )
         else:
-            self.text_label.config(text="Incorrect!", fg="red")
+            self.text_label.config(text="不正解！", fg="red")
             if self.sound_available:
                 winsound.PlaySound(
                     self.incorrect_sound, winsound.SND_FILENAME | winsound.SND_ASYNC
