1bf544b9dc20d8ad866780073ac97fda7f955f7f
tanakai0 <tanakatanakakai@gmail.com>
2023-09-06 01:47:44 +0900

    add widgets in mode selection screen

--- src/game_app.py
+++ src/game_app.py
@@ -1,26 +1,31 @@
 import platform
+import textwrap
 import tkinter as tk
 import winsound
 from tkinter import ttk
 
 from src.quiz import StaticQuizzes
+from src.quiz_database import DATABASE_FOLDER
 
 
 class TypingGameApp:
     N, W, E, S = tk.N, tk.W, tk.E, tk.S
+    ENDLESS_QUIZ = "Endless Quiz"
     TIME_LIMIT = "Time Limit"
     FIXED_QUIZ = "Fixed Quiz"
-    ENDLESS_QUIZ = "Endless Quiz"
 
     def __init__(self, root, database_path) -> None:
         self.database_path = database_path
-        self.root = root
-        self.root.title("Typing Game")
-        self.mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
         self.on_windows_os = platform.system() == "Windows"
         self.sound_available = self.on_windows_os
         self.widgets = []
         self.quizzes = StaticQuizzes(database_path)
+
+        self.root = root
+        self.root.title("Typing Game")
+        # self.root.geometry('800x400')
+        self.mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+
         if self.sound_available:
             self.correct_sound = "./assets/sound/pinpon2.wav"
             self.incorrect_sound = "./assets/sound/bubbu1.wav"
@@ -28,48 +33,80 @@ class TypingGameApp:
 
         self.question = ""
 
+        # widgets in the title screen
         self.title_label = tk.Label(
             self.mainframe, text="Typing Game", font=("Helvetica", 36)
         )
+        self.title_start_button = tk.Button(
+            self.mainframe, text="ゲームスタート", command=self.set_mode_selection_screen
+        )
 
+        # widgets in the mode selection screen
+        self.quiz_start_button = tk.Button(
+            self.mainframe, text="クイズ開始", command=self.push_quiz_start_button
+        )
         self.quiz_mode_label = tk.Label(
-            self.mainframe, text="Challenge\n Mode", font=("Helvetica", 12)
+            self.mainframe, text="クイズ\nモード", font=("Helvetica", 12)
         )
         self.quiz_mode = tk.StringVar(None, self.ENDLESS_QUIZ)
-        self.time_limit_radio_button = ttk.Radiobutton(
+        self.endless_quiz_radio_button = ttk.Radiobutton(
             self.mainframe,
-            text=self.TIME_LIMIT,
-            variable=self.quiz_mode,
-            value=self.TIME_LIMIT,
-        )
-        self.FIXED_QUIZ_radio_button = ttk.Radiobutton(
-            self.mainframe,
-            text=self.FIXED_QUIZ,
-            variable=self.quiz_mode,
-            value=self.FIXED_QUIZ,
-        )
-        self.ENDLESS_QUIZ_radio_button = ttk.Radiobutton(
-            self.mainframe,
-            text=self.ENDLESS_QUIZ,
+            text="エンドレス",
             variable=self.quiz_mode,
             value=self.ENDLESS_QUIZ,
         )
-
-        self.start_button = tk.Button(
-            self.mainframe, text="Start Game", command=self.push_start_button
+        self.time_limit_radio_button = ttk.Radiobutton(
+            self.mainframe,
+            text="時間制限",
+            variable=self.quiz_mode,
+            value=self.TIME_LIMIT,
+        )
+        self.fixed_quiz_radio_button = ttk.Radiobutton(
+            self.mainframe,
+            text="固定問題数",
+            variable=self.quiz_mode,
+            value=self.FIXED_QUIZ,
         )
 
+        self.max_seconds = tk.StringVar()
+        self.max_seconds_default = 60 * 2
+        self.max_seconds_limit = 60 * 60
+        self.max_seconds.set(self.max_seconds_default)
+        self.max_seconds_spinbox = tk.Spinbox(
+            self.mainframe,
+            from_=1,
+            to=self.max_seconds_limit,
+            increment=30,
+            textvariable=self.max_seconds,
+        )
+        self.max_num = tk.StringVar()
+        self.max_num_default = 10
+        self.max_num_limit = 10000
+        self.max_num.set(self.max_num_default)
+        self.max_num_spinbox = tk.Spinbox(
+            self.mainframe,
+            from_=1,
+            to=self.max_num_limit,
+            increment=1,
+            textvariable=self.max_num,
+        )
+        self.title_back_button = tk.Button(
+            self.mainframe, text="戻る", command=self.set_title_screen
+        )
+
+        # widgets in the quiz screen
         self.text_label = tk.Label(self.mainframe, text="", font=("Helvetica", 24))
-        self.user_input = tk.StringVar()
-        self.user_input_entry = tk.Entry(
-            self.mainframe, textvariable=self.user_input, font=("Helvetica", 18)
+        self.player_input = tk.StringVar()
+        self.player_input_entry = tk.Entry(
+            self.mainframe, textvariable=self.player_input, font=("Helvetica", 18)
         )
-        self.user_input_entry.bind("<Return>", self.check_text)
+        self.player_input_entry.bind("<Return>", self.check_text)
+        self.new_quiz()
 
         self.set_title_screen()
-        self.new_text()
 
     def set_title_screen(self) -> None:
+        self.unset_screen()
         self.mainframe.grid(row=0, column=0, sticky=(self.N, self.S, self.E, self.W))
         # self.resizable_mode()
         if self.sound_available:
@@ -78,10 +115,10 @@ class TypingGameApp:
                 winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP,
             )
         self.title_label.grid(column=0, row=0, columnspan=4, pady=20)
-        self.start_button.grid(column=0, row=5, columnspan=4, pady=5)
-        self.widgets += [self.title_label, self.start_button]
+        self.title_start_button.grid(column=0, row=5, columnspan=4, pady=5)
+        self.widgets += [self.title_label, self.title_start_button]
 
-    def unset_title_screen(self) -> None:
+    def unset_screen(self) -> None:
         for widget in self.widgets:
             widget.grid_forget()
         self.widgets = []
@@ -89,23 +126,80 @@ class TypingGameApp:
         if self.sound_available:
             winsound.PlaySound(None, winsound.SND_PURGE)
 
-    def push_start_button(self) -> None:
+    def push_quiz_start_button(self) -> None:
         quiz_mode = self.quiz_mode.get()
+        print(self.max_seconds.get())
+        print(
+            self.str2positive_int(
+                self.max_seconds.get(), self.max_seconds_default, self.max_seconds_limit
+            )
+        )
+        print(self.max_num.get())
+        print(
+            self.str2positive_int(
+                self.max_num.get(), self.max_num_default, self.max_num_limit
+            )
+        )
         match quiz_mode:
-            case self.TIME_LIMIT:
-                self.set_ENDLESS_QUIZ_screen()  # temp
-            case self.FIXED_QUIZ:
-                self.set_ENDLESS_QUIZ_screen()  # temp
             case self.ENDLESS_QUIZ:
-                self.set_ENDLESS_QUIZ_screen()
+                self.set_endless_quiz_screen()
+            case self.TIME_LIMIT:
+                self.set_endless_quiz_screen()  # temp
+            case self.FIXED_QUIZ:
+                self.set_endless_quiz_screen()  # temp
             case _:
                 raise ValueError(f"{quiz_mode} is invalid quiz mode.")
 
-    def set_ENDLESS_QUIZ_screen(self) -> None:
-        self.unset_title_screen()
+    def set_mode_selection_screen(self) -> None:
+        self.unset_screen()
+        db_paths = [p for p in DATABASE_FOLDER.glob("*.db")]
+        quizzes = [StaticQuizzes(p) for p in db_paths]
+        overview2index = {(q.name, q.description): q for q in quizzes}
+
+        # set table
+        name = "名前"
+        description = "説明"
+        column = (name, description)
+
+        # style = ttk.Style()
+        # style.configure("Treeview", rowheight=40)
+        quiz_table = ttk.Treeview(self.mainframe, columns=column, height=5)
+        quiz_table.column("#0", width=0, stretch="no")
+        quiz_table.column(name, anchor="w", width=200)
+        quiz_table.column(description, anchor="w", width=300)
+        quiz_table.heading("#0", text="")
+        quiz_table.heading(name, text=name, anchor="center")
+        quiz_table.heading(description, text=description, anchor="center")
+        for n, d in overview2index.keys():
+            wrapped_n = "\n".join(textwrap.wrap(n, width=20))
+            wrapped_d = "\n".join(textwrap.wrap(d, width=20))
+            quiz_table.insert(parent="", index="end", values=(wrapped_n, wrapped_d))
+        quiz_table.grid(column=0, row=0, columnspan=5, padx=10, pady=20)
+        self.quiz_mode_label.grid(column=0, row=1, rowspan=3)
+        self.endless_quiz_radio_button.grid(column=1, row=1, sticky=self.W)
+        self.time_limit_radio_button.grid(column=1, row=2, sticky=self.W)
+        self.fixed_quiz_radio_button.grid(column=1, row=3, sticky=self.W)
+        self.max_seconds_spinbox.grid(column=2, row=2, sticky=self.W)
+        self.max_num_spinbox.grid(column=2, row=3, sticky=self.W)
+        self.quiz_start_button.grid(column=4, row=1, rowspan=3)
+        self.title_back_button.grid(column=3, row=1, rowspan=3)
+        self.widgets += [
+            quiz_table,
+            self.quiz_mode_label,
+            self.endless_quiz_radio_button,
+            self.time_limit_radio_button,
+            self.fixed_quiz_radio_button,
+            self.max_seconds_spinbox,
+            self.max_num_spinbox,
+            self.title_back_button,
+            self.quiz_start_button,
+        ]
+
+    def set_endless_quiz_screen(self) -> None:
+        self.unset_screen()
         self.text_label.pack(pady=20)
-        self.user_input_entry.pack(pady=10)
-        self.user_input_entry.focus_set()
+        self.player_input_entry.pack(pady=10)
+        self.player_input_entry.focus_set()
 
     def resizable_mode(self) -> None:
         self.root.columnconfigure(0, weight=1)
@@ -116,13 +210,13 @@ class TypingGameApp:
         self.mainframe.columnconfigure(3, weight=1)
         self.mainframe.rowconfigure(1, weight=1)
 
-    def new_text(self) -> None:
+    def new_quiz(self) -> None:
         self.quiz = self.quizzes.generate_quiz()
         self.question = self.quiz.question
         self.text_label.config(text=self.question, fg="black")
 
     def check_text(self, event) -> None:
-        player_answer = self.user_input.get()
+        player_answer = self.player_input.get()
         if self.quiz.check_answer(player_answer):
             self.text_label.config(text="Correct!", fg="green")
             if self.sound_available:
@@ -135,5 +229,21 @@ class TypingGameApp:
                 winsound.PlaySound(
                     self.incorrect_sound, winsound.SND_FILENAME | winsound.SND_ASYNC
                 )
-        self.user_input.set("")
-        self.root.after(500, self.new_text)
+        self.player_input.set("")
+        self.root.after(500, self.new_quiz)
+
+    @staticmethod
+    def str2positive_int(s: str, default: int, max_limit: int) -> int:
+        try:
+            num = float(s)
+            if "." in s:
+                num = int(num)
+            if num > 0 and int(num) == num:
+                if num <= max_limit:
+                    return int(num)
+                else:
+                    return max_limit
+            else:
+                return default
+        except ValueError:
+            return default
