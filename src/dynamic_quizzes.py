9b483ad530c835e83190f4927226d626dbf69ec8
tanakai0 <tanakatanakakai@gmail.com>
2023-09-06 19:56:40 +0900

    prepare DOW dynamic quiz generator

--- src/dynamic_quizzes.py
+++ src/dynamic_quizzes.py
@@ -12,10 +12,14 @@ class DOWQuizzes(Quizzes):
             Whether to use proleptic Gregorian calendar.
         """
         name = "今日は何曜日？"
-        description = "西暦1年1月1日～西暦9999年12月31日から選ばれた日の曜日を当てよう！"
+        description = (
+            "西暦1年1月1日～西暦9999年12月31日から選ばれた日の曜日を当てよう！"
+            + "ただし、西暦1582年10月4日以前はユリウス暦、"
+            + "西暦1582年10月15日以降はグレゴリオ暦として答えてね．ちなみに、その間の日付は問題には出さないよ。"
+        )
         super().__init__(name, description)
 
-    def generate_quiz():
+    def generate_quiz(self):
         pass
 
 
